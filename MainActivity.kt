ef718386e55690dce92faebbd33d2e0a1c8a7b81
chunghihi11 <101981189+chunghihi11@users.noreply.github.com>
2023-07-12 11:46:34 +0530

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,22 @@
+package org.meicode.securemessageapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val BtnLogin = findViewById<Button>(R.id.BtnLogin)
+
+        BtnLogin.setOnClickListener{
+
+            intent = Intent(this, LoginActivity::class.java )
+            startActivity(intent)
+        }
+    }
+}
